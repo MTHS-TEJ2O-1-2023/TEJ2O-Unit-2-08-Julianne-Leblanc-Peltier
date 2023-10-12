84bfee469d91e2a91166fc8ac80c6f33bb653edd
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-12 11:42:53 +0000

    la fin

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,53 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Julianne Leblanc-Peltier
+Created on: Sep 2023
+This module is a Micro:bit MicroPython program that displays colours using an RBG LED
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
+pin8.write_digital(0)
+pin12.write_digital(0)
+pin16.write_digital(0)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        pin8.write_digital(1)
+        pin12.write_digital(1)
+        pin16.write_digital(1)
+        display.scroll("White")
+        pin8.write_digital(0)
+        pin12.write_digital(0)
+        pin16.write_digital(0)
+
+        pin8.write_digital(1)
+        display.scroll("Red")
+        pin8.write_digital(0)
+
+        pin12.write_digital(1)
+        display.scroll("Green")
+        pin12.write_digital(0)
+
+        pin16.write_digital(1)
+        display.scroll("Blue")
+        pin16.write_digital(0)
+
+        pin8.write_digital(1)
+        pin12.write_digital(1)
+        display.scroll("Yellow")
+        pin8.write_digital(0)
+        pin12.write_digital(0)
+
+        pin8.write_digital(1)
+        pin16.write_digital(1)
+        display.scroll("Magenta")
+        pin8.write_digital(0)
+        pin16.write_digital(0)
+
+        pin12.write_digital(1)
+        pin16.write_digital(1)
+        display.scroll("Cyan")
+        pin12.write_digital(0)
+        pin16.write_digital(0)
